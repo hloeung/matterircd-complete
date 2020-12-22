55c63b1a349e7923649b8b2617618131ba419ce9
Haw Loeung <haw.loeung@canonical.com>
2020-12-23 10:27:19 +1100

    Allow tab-complete message/thread IDs without '@@' but only if first word on line

--- matterircd_complete.pl
+++ matterircd_complete.pl
@@ -256,15 +256,20 @@ signal_add_last 'gui key pressed' => sub {
 signal_add_last 'complete word' => sub {
     my ($complist, $window, $word, $linestart, $want_space) = @_;
 
+    # We only want to tab-complete message/thread if this is the first
+    # word on the line.
+    return if $linestart;
     return unless settings_get_int('matterircd_complete_message_thread_id_cache_size');
-    return unless substr($word, 0, 2) eq '@@';
+    return if (substr($word, 0, 1) eq '@' and substr($word, 0, 2) ne '@@');
     return unless $window->{active} and ($window->{active}->{type} eq 'CHANNEL' || $window->{active}->{type} eq 'QUERY');
     return unless exists($MSGTHREADID_CACHE{$window->{active}->{name}});
 
     my %chatnets = map { $_ => 1 } split(/\s+/, settings_get_str('matterircd_complete_networks'));
     return unless exists $chatnets{'*'} || exists $chatnets{$window->{active_server}->{chatnet}};
 
-    $word = substr($word, 2);
+    if (substr($word, 0, 2) eq '@@') {
+        $word = substr($word, 2);
+    }
 
     foreach my $msgthread_id (@{$MSGTHREADID_CACHE{$window->{active}->{name}}}) {
         if ($msgthread_id =~ /^\Q$word\E/) {
