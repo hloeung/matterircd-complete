795e6b72370142cd4c45b47003b21fde6e5778d7
Haw Loeung <haw.loeung@canonical.com>
2022-12-07 09:56:39 +1100

    Revert 4eb0ad7 and exclude reactions from the cache
    We can't reply to reactions nor can we react to them so there's no
    point having them in the cache. We can include the parent/thread IDs
    for reactions once matterircd includes those.

--- matterircd_complete.pl
+++ matterircd_complete.pl
@@ -513,6 +513,12 @@ sub cache_msgthreadid {
         }
     }
 
+    # We also want to ignore reactions as we can't reply to those
+    # directly if they're to a message in a thread.
+    if ($msg =~ /(?:added|removed) reaction:/) {
+        return;
+    }
+
     # Mattermost message/thread IDs.
     if ($msg =~ /\[(?:->|↪)?\@\@([0-9a-z]{26})(?:,\@\@([0-9a-z]{26}))?\]/) {
         my $msgthreadid = $1;
